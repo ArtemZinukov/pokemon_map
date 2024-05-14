8a68514e18f3a2608fe86a1937fdf34be727f133
Artem Zinukov <zinykov_a@mail.ru>
2024-05-14 23:01:25 +0300

    Made the fields optional

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,8 +5,8 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200, null=True, verbose_name="имя покемона")
     photo = models.ImageField(null=True, blank=True, verbose_name="фото")
     description = models.TextField(null=True, blank=True, verbose_name="описание")
-    title_en = models.CharField(max_length=200, null=True, verbose_name="имя покемона на англ.")
-    title_jp = models.CharField(max_length=200, null=True, verbose_name="имя покемона на японском")
+    title_en = models.CharField(max_length=200, null=True, blank=True, verbose_name="имя покемона на англ.")
+    title_jp = models.CharField(max_length=200, null=True, blank=True, verbose_name="имя покемона на японском")
     previous_evolution = models.ForeignKey("self",
                                            verbose_name="из кого эволюционирует",
                                            on_delete=models.CASCADE,
@@ -21,8 +21,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entity', verbose_name="имя покемона")
     lat = models.FloatField(verbose_name="широта")
     lon = models.FloatField(verbose_name="долгота")
-    appeared_at = models.DateTimeField(verbose_name="появится в")
-    disappeared_at = models.DateTimeField(verbose_name="пропадет в")
+    appeared_at = models.DateTimeField(null=True, verbose_name="появится в")
+    disappeared_at = models.DateTimeField(null=True, verbose_name="пропадет в")
     level = models.IntegerField(verbose_name="уровень")
     health = models.IntegerField(verbose_name="здоровье")
     strength = models.IntegerField(verbose_name="сила")
